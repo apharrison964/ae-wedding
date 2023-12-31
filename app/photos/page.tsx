c5bf2cbc6d03273b1ecec41cd4972988500e842c
Alexandra Harrison <apharrison964@gmail.com>
2023-12-30 21:54:49 -0500

    adding fake space

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,30 +1,46 @@
 import styles from '../../styles/common.module.scss';
 import React from 'react';
-// import { Amplify, API, graphqlOperation } from 'aws-amplify'
-// import awsExports from '../../src/aws-exports';
-// import { listAttendees } from '../../src/graphql/queries';
-// import { ListAttendeesQuery } from '../../src/API';
-// import { GraphQLResult } from "@aws-amplify/api";
-
-// Amplify.configure(awsExports);
 
 const Photos = async () => {
-    // const attendeeData = (await API.graphql(graphqlOperation(listAttendees))) as GraphQLResult<ListAttendeesQuery>;
-    // const attendees = attendeeData.data?.listAttendees?.items;
     return (
         <>
             <h3 className={styles.secondary}>
             Photos - Coming Soon!
             </h3>
-
-            {/* <div className={styles.grid}>
-            {attendees?.map((attendee) => (
-                <a className={styles.card} key={attendee?.id}>
-                <h3>{attendee?.name}</h3>
-                <p>{attendee?.food}</p>
-                </a>
-            ))}
-            </div> */}
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
+            <div style={{margin: '0.5rem'}}>
+            </div>
         </>
     );
 }
